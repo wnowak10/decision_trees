1e8b2ccaf5520bcb2aaab739c5a1e177ffb4a318
wnowak <wnowak10@gmail.com>
2016-12-24 17:22:32 -0500

    sort data

--- IGtree.py
+++ IGtree.py
@@ -7,3 +7,11 @@ eps=sys.float_info.epsilon
 os.chdir('/Users/wnowak/Desktop') # Provide the path here
 df = pd.read_csv("df.csv")
 
+
+#what does df look like?
+print(df)
+print(df.dtypes)
+
+# sort by categorical variable
+cover = df.sort_values(by="Cloud cover")
+print(cover)
